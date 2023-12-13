3b6d5a9e8cc8b1dce52b309e7e3c657121849874
Bruno Martins <bgcngm@gmail.com>
2023-12-13 01:03:15 +0000

    [DNM] drivers: dwc3: Reset to KERNEL.PLATFORM.2.0.r1-15300-kernel.0
    Change-Id: Ie7f94bbee7c1f700f7dc579540d545e748bb1900

--- drivers/usb/dwc3/dwc3-qcom.c
+++ drivers/usb/dwc3/dwc3-qcom.c
@@ -306,16 +306,7 @@ static void dwc3_qcom_interconnect_exit(struct dwc3_qcom *qcom)
 /* Only usable in contexts where the role can not change. */
 static bool dwc3_qcom_is_host(struct dwc3_qcom *qcom)
 {
-	struct dwc3 *dwc;
-
-	/*
-	 * FIXME: Fix this layering violation.
-	 */
-	dwc = platform_get_drvdata(qcom->dwc3);
-
-	/* Core driver may not have probed yet. */
-	if (!dwc)
-		return false;
+	struct dwc3 *dwc = platform_get_drvdata(qcom->dwc3);
 
 	return dwc->xhci;
 }
