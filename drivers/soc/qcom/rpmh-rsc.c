3f9cb367397578e015a3f246fedfae6820ced0c3
Bruno Martins <bgcngm@gmail.com>
2024-11-01 11:22:10 +0000

    fix
    Change-Id: Ic686a38620128089e9c7bac7fe040ab3d65b1d0b

--- drivers/soc/qcom/rpmh-rsc.c
+++ drivers/soc/qcom/rpmh-rsc.c
@@ -792,7 +792,7 @@ int rpmh_rsc_send_data(struct rsc_drv *drv, const struct tcs_request *msg, int c
 
 	/* Controller is busy in 'solver' mode */
 	if (drv->in_solver_mode) {
-		spin_unlock_irqrestore(&drv->lock, flags);
+		spin_unlock_irqrestore(&drv->lock);
 		return -EBUSY;
 	}
 
@@ -835,7 +835,7 @@ int rpmh_rsc_send_data(struct rsc_drv *drv, const struct tcs_request *msg, int c
 	if (!msg->wait_for_compl)
 		clear_bit(tcs_id, drv->tcs_in_use);
 
-	spin_unlock_irqrestore(&drv->lock, flags);
+	spin_unlock_irqrestore(&drv->lock);
 
 	if (!msg->wait_for_compl)
 		wake_up(&drv->tcs_wait);
