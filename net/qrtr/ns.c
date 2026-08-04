57c75d68c54d60088e173fa3cb8f7f6c0ca36880
Lukasz Patron <priv.luk@gmail.com>
2026-08-04 22:12:12 +0200

    net: qrtr: ns: Raise lookup limit to 128
    Current limit of 64 is not enough for Sony Xperia 10 VII (SM6475).
    
    After merging v6.6.142 into a downstream AOSP device, it's stuck on
    boot animation and following log spam can be observed in dmesg:
    
    E qrtr    : ctrl_cmd_new_lookup(): QRTR client node exceeds max lookup limit!
    E qrtr    : qrtr_ns_worker(): failed while handling packet from 1:16600
    
    No idea why it needs more than 64 client lookups, but it appears to
    work fine with 128 as it did when there were no limits.
    
    I don't really have a good way to investigate what it needs all
    these lookups for as most of the userspace is closed source.
    
    Fixes: 5640227d9a21 ("net: qrtr: ns: Limit the maximum number of lookups")
    Change-Id: I303cb6d8672c05262ad0532664fbfb8ea989891e
    Signed-off-by: Łukasz Patron <priv.luk@gmail.com>
    Link: https://patch.msgid.link/20260804201919.1148015-1-priv.luk@gmail.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- net/qrtr/ns.c
+++ net/qrtr/ns.c
@@ -81,7 +81,7 @@ struct qrtr_node {
 /* Max lookup limit is chosen based on the current platform requirements. If the
  * requirement changes in the future, this value can be increased.
  */
-#define QRTR_NS_MAX_LOOKUPS 64
+#define QRTR_NS_MAX_LOOKUPS 128
 
 /* Max nodes, server, lookup limits are chosen based on the current platform
  * requirements. If the requirement changes in the future, these values can be
